d01d491db87a6877347901327818467a1ebc7177
KuzIlya <KuznetsovIlyaDM@yandex.ru>
2024-09-14 16:45:35 +0300

    fffdfdf

--- app/core/config.py
+++ app/core/config.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from pydantic import EmailStr
 from pydantic_settings import BaseSettings
 
@@ -7,8 +9,8 @@ class Settings(BaseSettings):
     app_description: str = 'Благотворительный фонд поддержки котиков'
     database_url: str = 'sqlite+aiosqlite:///./fastapi.db'
     secret: str = 'SECRET'
-    first_superuser_email: EmailStr | None = None
-    first_superuser_password: str | None = None
+    first_superuser_email: Optional[EmailStr] = None
+    first_superuser_password: Optional[str] = None
 
     class Config:
         env_file = '.env'
