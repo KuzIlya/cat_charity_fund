b59fd31c4f91b8a7b5984aafc164441758a108a6
KuzIlya <KuznetsovIlyaDM@yandex.ru>
2024-09-14 17:21:16 +0300

    blyat

--- app/utils/investing.py
+++ app/utils/investing.py
@@ -10,7 +10,7 @@ from app.models import CharityProject, Donation
 async def get_not_full_invested_objects(
     obj_in: Union[CharityProject, Donation],
     session: AsyncSession
-) -> List[CharityProject, Donation]:
+) -> List[Union[CharityProject, Donation]]:
     objects = await session.execute(
         select(obj_in).where(obj_in.fully_invested == 0
                              ).order_by(obj_in.create_date)
